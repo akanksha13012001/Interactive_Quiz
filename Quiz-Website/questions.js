2f532f69a0b05159d549e8ed12e7c1a9746bef52
akanksha13012001 <gadbailakanksha@gmail.com>
2025-05-07 21:01:24 +0530

    Add files via upload

---
+++ Quiz-Website/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which HTML attribute is used to define inline styles?",
+        answer: "B. style",
+        options: [
+            "A. class",
+            "B. style",
+            "C. font",
+            "D. styles"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which CSS property controls the text size?",
+        answer: "A. font-size",
+        options: [
+            "A. font-size",
+            "B. text-style",
+            "C. text-size",
+            "D. font-style"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which property is used to change the text color in CSS?",
+        answer: "C. color",
+        options: [
+            "A. bgcolor",
+            "B. font-color",
+            "C. color",
+            "D. text-color"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which CSS property is used to set the background color?",
+        answer: "B. background-color",
+        options: [
+            "A. color",
+            "B. background-color",
+            "C. bgcolor",
+            "D. bg-color"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which company developed JavaScript?",
+        answer: "A. Netscape",
+        options: [
+            "A. Netscape",
+            "B. Microsoft",
+            "C. Sun Microsystems",
+            "D. IBM"
+        ]
+    }
+];
